b06088f781a6d2c1521d5632fadd49762bf171c3
siliconmagic <siliconmagic@126.com>
2019-04-05 19:51:46 +0800

    min_s commit

---
+++ min_s.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+
+int function_1(int *ptr_1, char *ptr_2){
+	if(*ptr_1 == 3 && *ptr_2 == 'A'){
+		std::cout<<"hit this hole!"<<std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int main(){
+	int a = 3;
+	char b = 'B';
+	function_1(&a, &b);
+	return 0;
+}
